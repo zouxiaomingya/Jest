12fe23130dc310d3c039449fcae0a705a2539743
zouxiaomingya <13870939865@163.com>
2019-10-13 13:19:00 +0800

    [fix]

--- src/async.js
+++ src/async.js
@@ -1,3 +1,9 @@
 export const fetchApple = (callback) => {
   setTimeout(() => callback('apple'), 300)
 }
+
+export const fetchBanana = () => {
+  return new Promise((resolve, reject) => {
+    resolve('banana')
+  })
+}
--- test/async.test.js
+++ test/async.test.js
@@ -1,11 +1,16 @@
-import { fetchApple } from '../src/async'
+import { fetchApple, fetchBanana } from '../src/async'
 
 test('the data is apple', (done) => {
+  console.log(done);
   expect.assertions(1)
   const callback = data => {
     expect(data).toBe('apple')
     done()
   }
-
   fetchApple(callback)
+})
+
+test('the data is banana', () => {
+  expect.assertions(1)
+  return fetchBanana().then(data => expect(data).toBe('banana'))
 })
